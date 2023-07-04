6e9280b38ea21ac387488424c472eeae76022c64
KHB2937 <gmlqja7913@gmail.com>
2023-07-04 15:47:43 +0900

    네이버지도 크롤링

--- 네이버지도.py
+++ 네이버지도.py
@@ -33,8 +33,64 @@ time.sleep(2)
 search_input.send_keys(Keys.ENTER)
 time.sleep(5)
 
+# iframe으로 전환
+iframe_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'searchIframe')))
+driver.switch_to.frame(iframe_element)
+
+# 요소 찾기
+ul_element = driver.find_element(By.TAG_NAME, 'ul')
+li_element = ul_element.find_element(By.XPATH, './/li[1]')
+div_element = li_element.find_element(By.XPATH, './/div')
+a_element = div_element.find_element(By.XPATH, './/a')
+
+# 요소 클릭
+a_element.click()
+time.sleep(3)
+
+# iframe에서 벗어나기
+driver.switch_to.default_content()
+
+# iframe으로 전환
+iframe_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'entryIframe')))
+driver.switch_to.frame(iframe_element)
+
+#가게명
+span_element = driver.find_element(By.XPATH, '//*[@id="_title"]/span[1]')
+name = span_element.text
+
+# 주소
+span_element = driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[1]/div/a/span[1]')
+address = span_element.text
+
+#전화번호
+span_element = driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[6]/div/div[2]/div/div/div[4]/div/span[1]')
+number = span_element.text
+
+# 별점
+try:
+    star_element = driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[2]/div[1]/div[2]/span[1]/em')
+    star = star_element.text
+except NoSuchElementException:
+    star = 'NaN'
+
+review_element = driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[5]/div/div/div/div/a[5]/span')
+review_element.click()
+time.sleep(2)
+try:
+    reviNum_element = driver.find_element(By.CSS_SELECTOR, '#app-root > div > div > div > div:nth-child(7) > div:nth-child(2) > div:nth-child(3) > h2 > span.place_section_count')
+    reviNum = reviNum_element.text
+except NoSuchElementException:
+    review = 'NaN'
 
 
+
+# 텍스트 출력
+print(name)
+print(address)
+print(number)
+print(star)
+print(review)
+print('-------------------')
 
 # 프로그램을 종료하려면 사용자의 입력을 기다립니다.
 input("Press Enter to exit...")
